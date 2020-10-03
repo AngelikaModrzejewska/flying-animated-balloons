4f7a41e325bbe6cea60a477dc760836ab5395c1d
Angelika Modrzejewska <ang.modrzejewska@gmail.com>
2020-10-03 21:13:45 +0200

    Updates

---
+++ precache-manifest.945cf42986b30fe25bc99c84d5f909be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be2d2017a68e8e8309f884c3bdbf618",
+    "url": "./index.html"
+  },
+  {
+    "revision": "297b5a79ee589e2e956e",
+    "url": "./static/css/main.537caed5.chunk.css"
+  },
+  {
+    "revision": "201cae97766ad44bbccb",
+    "url": "./static/js/2.b0696c7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b0696c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297b5a79ee589e2e956e",
+    "url": "./static/js/main.69baf97b.chunk.js"
+  },
+  {
+    "revision": "7921114ab38779842383",
+    "url": "./static/js/runtime-main.c9e5ecf1.js"
+  }
+]);
